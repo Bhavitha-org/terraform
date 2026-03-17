bb7823c34e6a65583ff0f152a98cb4c6d9ecbe62
Bhavitha <lbhavitha342@gmail.com>
2026-03-17 08:13:18 +0530

    ansible

--- for_each/variables.tf
+++ for_each/variables.tf
@@ -1,9 +1,24 @@
+#MAP
+# variable "instances" {
+#     type = map(string)
+#     default = {
+#         mongodb = "t3.micro"
+#         mysql = "t3.small"
+#         shipping = "t3.small"
+#         user = "t3.micro"
+#     } 
+# }
+
+#this list should be converted into set
 variable "instances" {
-    type = map(string)
-    default = {
-        mongodb = "t3.micro"
-        mysql = "t3.small"
-        shipping = "t3.small"
-        user = "t3.micro"
-    } 
+    type = list 
+    default = ["mongodb", "shipping"]
+}
+
+variable "zone_id" {
+    default = "Z047633137OO0E33GOO9K"
+}
+
+variable "domain_name" {
+    default = "exploreops.online"
 }
